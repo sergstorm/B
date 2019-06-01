d7c1f290740138c4a78757ea483c2f1ec79a5cbe
login <sergstorm2011@yandex.ru>
2019-06-01 19:54:44 -0300

    hw

--- Collections/Pr.java
+++ Collections/Pr.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package Collections;
 import java.util.*;
 
@@ -60,3 +61,67 @@ public class Pr {
         }
     }
 }
+=======
+package Collections;
+import java.util.*;
+
+public class Pr {
+   static String []words = new String[20];
+    public static void main(String[] args) {
+        setWords(words);
+        getWords();
+        Set<String> set = new HashSet<String>();
+        Set<String> treeset = new TreeSet<String>(Arrays.asList(words));
+        System.out.print("\nAFTER SORT : ");
+        Collections.sort(Arrays.asList(words));
+        System.out.println();
+        getWords();
+        set.addAll(Arrays.asList(words));
+        System.out.println();
+        System.out.println("SET : "+set);
+        System.out.println("TREESET : "+treeset);
+        freqwords();
+
+        TelBook telBook  = new TelBook();
+        telBook.addNumber(2323,"A");
+        telBook.addNumber(1231,"A");
+        telBook.addNumber(1111,"B");
+        telBook.addNumber(1155,"B");
+        telBook.addNumber(1116,"B");
+        telBook.addNumber(2222,"C");
+        System.out.println();
+        telBook.getNumber("A");
+        telBook.getNumber("B");
+        telBook.getNumber("C");
+
+    }
+
+    public static void setWords(String[] words) {
+              //
+        for (int i = 0; i <words.length ; i++) {
+
+            StringBuilder b = new StringBuilder();
+            for (int j = 0; j <5 ; j++)
+            {
+                int r = (int)(Math.random()*2);
+                b =b.append(String.valueOf ((char)(j+65+r)));
+            }
+            words[i] = b.toString();
+        }
+    }
+
+    public static void getWords() {
+        System.out.print("WORDS : ");
+        for (int i = 0; i <words.length ; i++) {
+            System.out.print((i+1)+"# "+words[i]);
+        }
+    }
+    public static void freqwords()
+    {
+        for (int i = 0; i <words.length ; i++)
+        {
+            System.out.print(" Freq "+Collections.frequency(Arrays.asList(words),words[i])+" "+words[i]);
+        }
+    }
+}
+>>>>>>> hw
--- Collections/TelBook.java
+++ Collections/TelBook.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package Collections;
 
 import java.util.HashMap;
@@ -20,3 +21,27 @@ public class TelBook {
         }
     }
 }
+=======
+package Collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TelBook {
+    HashMap<Integer,String> hashMap = new HashMap<>();
+
+    public HashMap addNumber(int number,String surname)
+    {
+        hashMap.put(number,surname);
+        return hashMap;
+    }
+    public void getNumber(String name)
+    {
+        for (Map.Entry<Integer,String> p:hashMap.entrySet())
+        {
+            if (p.getValue().equals(name))
+            System.out.println(p.getKey() +" VALUE "+ p.getValue());
+        }
+    }
+}
+>>>>>>> hw
